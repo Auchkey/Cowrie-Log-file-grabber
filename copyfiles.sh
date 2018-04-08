d0f549e4a07e75e03d8dff624ee8e15b322e2070
Max C <Auchkey@users.noreply.github.com>
2018-04-08 15:27:12 +0000

    Improved log file identifier
    Added two functions "getTTYLogFiles", and "playTTYLogFiles"
    
    getTTYLogFiles: uses egrep to match TTY log files and puts them in an array.
    
    playTTYLogFiles: loops the array, playing each log file and also outputting the file name and number. This will be useful later for parsing user regex input and putting matched files into a separate directory or whatever else may be used to ease examining them later.

--- copyfiles.sh
+++ copyfiles.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
-#UP819474
-#Copies event log files matching regex into /cowrie/log/matchedtty/*foldername*
-
-#mkdir -p ../log/matchedtty/test
-
-cd ../log/tty
-
-ls -l | grep -E +.log$ | cut -c 45-79 | xargs python ../../bin/playlog -m 0
+# UP819474
+# Copies event log files matching regex into /cowrie/log/matchedtty/*foldername*
+# TODO: get regex input from user, scan TTYlogs and put files with matched content in seperate directory for easy examination later.
 
 
-# list directory, extended grep (for regex) to only return .log files, cut to file name (which is always characters 45 to 79), then play each log
+# mkdir -p ../log/matchedtty/test
+# cd ../log/tty
 
-#"playlog -m 0" = instant ouput
 
-# must cut to only file name: current -l prints sizes, dates, permissions etc...
+# Place all valid TTY .log files into an array
+function getTTYLogFiles() {
 
+	TTYLogArray=() # declare array
+	while IFS= read -r line; do
+		TTYLogArray+=( "$line" ) # each line is a new array element
+	done < <( ls | egrep '[0-9]{8}-[0-9]{6}-.{12}-.{2,3}.log' ) # ls and grep instead of find to keep logs in order of date
+	
+}
+
+
+
+function playTTYLogFiles() {
+	
+	arraylength=${#TTYLogArray[@]} # get length of array
+	for (( i=0; i<${arraylength}; i++ )); do # step through array
+		printf "\nFile name: ${TTYLogArray[$i]}"
+		printf "\nFile number: $[$i+1]\n"
+		python playlog -m 0 ${TTYLogArray[$i]} # play logs with no input delay
+	
+	done
+
+}
+
+(getTTYLogFiles && playTTYLogFiles) || echo "There was an error." # will hopefully be more helpful later
+
+
+# grep -H "text" "file" = return file name of matched string
+
+# xargs grep -H "text" = as above
+
+# Must use playlog as .log files are not in format parsable with tools like cat
+
+#? tests exit code of last command (0 for success, 1 for failure)
 
